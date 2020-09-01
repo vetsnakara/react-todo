fc48d020b5dd796d65b82bc154f8ed653af47daa
Konstantin Arakantsev <kostya3312@gmail.com>
2020-09-01 18:46:30 +0300

    add basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
+import { SvgIcon } from './Icon'
+
 const App = () => {
   return (
-    <div>Hello world</div>
+    <div className="container">
+      <div className="todo">
+        <div className="todo__sidebar">
+          <SvgIcon width={40} height={40} name="add" />
+          <SvgIcon width={40} height={40} name="check" />
+          <SvgIcon width={40} height={40} name="close" />
+          <SvgIcon width={40} height={40} name="edit" />
+          <SvgIcon width={40} height={40} name="list" />
+          <SvgIcon width={40} height={40} name="remove" />
+          Sidebar
+      </div>
+        <div className="todo__tasks">
+          Tasks
+      </div>
+      </div>
+    </div>
   );
 }
 
